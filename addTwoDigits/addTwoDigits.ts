aa85215848eb0f5dfe870e408df015dabe32d923
Sarkis-S <Sarkis.Sos19@gmail.com>
2020-03-03 18:57:54 -0800

    Complete addTwoDigits.ts

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,13 @@
 function addTwoDigits(n: any): number {
+  // Turn n into string then split into array
+  // Use higher order function reduce to tally up
+    // We will need to parse the number string
 
+  let numArray = n.toString().split('');
+
+  return numArray.reduce((a: string, b: string) => {
+    return parseInt(a) + parseInt(b);
+  });
 }
 
-// console.log(addTwoDigits(29));
+console.log(addTwoDigits(29));
