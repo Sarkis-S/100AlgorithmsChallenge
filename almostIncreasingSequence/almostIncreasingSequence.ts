9f8341bc09bff44a0dc766f7dfffb7a552c7beca
Sarkis-S <Sarkis.Sos19@gmail.com>
2020-03-08 21:18:25 -0700

    Complete almostIncreasingSequence.ts

--- almostIncreasingSequence/almostIncreasingSequence.ts
+++ almostIncreasingSequence/almostIncreasingSequence.ts
@@ -1,6 +1,30 @@
 function almostIncreasingSequence(sequence: number[]): boolean {
+  // We need to keep count of errors
 
+  // Iterate through array
+  // Compare current with previous if smaller
+    // Increment count
+  // Compare current with pre-previous AND next with prevous
+    // Return false
+  
+  // Return errors less than or equal to 1
+
+  let count = 0;
+
+  for (let i = 0; i < sequence.length; i++) {
+    let current = sequence[i];
+    let prev = sequence[i-1];
+    let next = sequence[i+1];
+
+    if (current <= prev) {
+      count++;
+      if (current <= sequence[i-2] && next <= prev) {
+        return false;
+      }
+    }
+  }
+  return count <= 1;
 }
 
-// console.log(almostIncreasingSequence([1, 3, 2, 1])) 
-// console.log(almostIncreasingSequence([1, 3, 2])) 
+console.log(almostIncreasingSequence([1, 3, 2, 1])) // false
+console.log(almostIncreasingSequence([1, 3, 2])) // true
