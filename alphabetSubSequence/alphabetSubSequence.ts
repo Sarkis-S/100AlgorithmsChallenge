32ce11318e1956764e9d0b8b884c2a10927e5bd5
Sarkis-S <Sarkis.Sos19@gmail.com>
2020-03-10 15:25:46 -0700

    Complete alphabetSubSequence.ts

--- alphabetSubSequence/alphabetSubSequence.ts
+++ alphabetSubSequence/alphabetSubSequence.ts
@@ -1,9 +1,53 @@
 function alphabetSubsequence(s: string): boolean {
+    // We need the alphabet to check the index of characters against
+    // We need a counter for duplicate characters
+    // We need to split the string into an array
 
+    // Loop through the array
+      // up the character count
+      // if the index of current char is greather than the next
+        // return false
+      // if its a duplicate char
+        // return false
+    
+    // All other case return true
+
+
+  const alphabet = [
+    'a', 'b', 'c', 'd', 'e', 
+    'f', 'g', 'h', 'i', 'j', 
+    'k', 'l', 'm', 'n', 'o', 
+    'p', 'q', 'r', 's', 't', 
+    'u', 'v', 'w', 'x', 'y', 'z'];
+
+  const noDups = {};
+  const chars: string[] = s.split('');
+
+  for (let i = 0; i < chars.length - 1; i++) {
+    let current = chars[i];
+    let next = chars[i+1];
+
+    // handles dups by keeping count
+    if (!noDups[current]) {
+      noDups[current] = 1;
+    } else {
+      noDups[current]++;
+    }
+
+    // conditions for returning false
+    if (alphabet.indexOf(current) > alphabet.indexOf(next)) {
+      return false;
+    } else if (noDups[current] === 2) {
+      return false;
+    }
+  }
+  
+  // otherwise return true
+  return true;
 }
 
-console.log(alphabetSubsequence('zab'))
-console.log(alphabetSubsequence('effg'))
-console.log(alphabetSubsequence('cdce'))
-console.log(alphabetSubsequence('ace'))
-console.log(alphabetSubsequence('bxz'))
+console.log(alphabetSubsequence('zab')); // false
+console.log(alphabetSubsequence('effg')); // false
+console.log(alphabetSubsequence('cdce')); // false
+console.log(alphabetSubsequence('ace')); // true
+console.log(alphabetSubsequence('bxz')); // true
