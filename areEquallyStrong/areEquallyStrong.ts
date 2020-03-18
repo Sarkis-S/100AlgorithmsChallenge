b3a6e88ea27bf0e200a0fa46ada77c5b7fe2762e
Sarkis-S <Sarkis.Sos19@gmail.com>
2020-03-17 21:12:44 -0700

    Complete areEquallyStrong.ts

--- areEquallyStrong/areEquallyStrong.ts
+++ areEquallyStrong/areEquallyStrong.ts
@@ -1,7 +1,25 @@
 function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
+  // Bizzare problem but amusing!
+  // We want to compare each others arms and check that the numbers add up equally
+  // This is pure logic
 
+  let myArm = [yourLeft, yourRight];
+  let friendsArm = [friendsLeft, friendsRight]
+  let isEquallyStrong = false;
+  // Check that your arms value are also found in friend and vice versa
+  if (myArm[0] === friendsArm[0] || myArm[0] === friendsArm[1]) {
+    if (myArm[1] === friendsArm[0] || myArm[0] === friendsArm[1]) {
+      isEquallyStrong = true;
+    }
+  }
+  // Checks that you and your friends stength values are equal
+  if (myArm[0] + myArm[1] === friendsArm[0] + friendsArm[1]) {
+    isEquallyStrong = true;
+  }
+
+  return isEquallyStrong;
 }
 
-// console.log(areEquallyStrong(10, 15, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 9))
+console.log(areEquallyStrong(10, 15, 15, 10)); // true
+console.log(areEquallyStrong(15, 10, 15, 10)); // true
+console.log(areEquallyStrong(15, 10, 15, 9)); // false
