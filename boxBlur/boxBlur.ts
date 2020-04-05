a09ae30e27bc41a57d7073e8b8fe4f2c90d9c0d3
Sarkis-S <Sarkis.Sos19@gmail.com>
2020-04-04 17:06:13 -0700

    Fix bug. Now reads all values

--- boxBlur/boxBlur.ts
+++ boxBlur/boxBlur.ts
@@ -20,25 +20,30 @@ function boxBlur(image: number[][]): number[][] {
             // Create loops to target y-axis values
                 // Create another loop to target x values within the y-axis
 
-
+    // return  output resolution
 
 
     const outputRes = [];
 
     for (let y = 0; y < image.length - 2; y++) {
-        console.log(image[y]);
+        const line = [];
 
         for (let x = 0; x < image[y].length - 2; x++) {
-            console.log(image[y][x]);
+            let sum = 0;
+            let count = 0;
             
-            for (let a = y; a < y + 3; y++) {
-
+            for (let a = y; a < y + 3; a++) {
                 for (let b = x; b < x + 3; b++) {
-                    
-                }
+                    sum += image[a][b];
+                    count++;
 
+                }
             }
+
+            line.push(Math.floor(sum/count));
         }
+
+        outputRes.push(line);
     }
 
     return outputRes;
