481091f67b13c40a6d001487a6e58d9c80bffedf
Sarkis-S <Sarkis.Sos19@gmail.com>
2020-04-04 17:11:08 -0700

    Complete boxBlur.ts

--- boxBlur/boxBlur.ts
+++ boxBlur/boxBlur.ts
@@ -31,12 +31,11 @@ function boxBlur(image: number[][]): number[][] {
         for (let x = 0; x < image[y].length - 2; x++) {
             let sum = 0;
             let count = 0;
-            
+
             for (let a = y; a < y + 3; a++) {
                 for (let b = x; b < x + 3; b++) {
                     sum += image[a][b];
                     count++;
-
                 }
             }
 
