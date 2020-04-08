5d61852ede42cc61a8a59b10cedcab95cd0ef4d5
Sarkis-S <Sarkis.Sos19@gmail.com>
2020-04-07 23:28:14 -0700

    Complete candies.ts

--- candies/candies.ts
+++ candies/candies.ts
@@ -1,5 +1,12 @@
 function candies(n: number, m: number): number {
+  // n = children, m = pieces of candy
+  // We take the number of candies (m) divide that by number of children (n)
+  // We need to use Math.floor to round the answer down
+  // We take the output and multiply it by n
 
+  let numOfCandies = Math.floor(m/n);
+  return numOfCandies * n;
 }
 
-console.log(candies(3, 10));
+console.log(candies(3, 10)); // 9
+console.log(candies(4, 17)); // 16
