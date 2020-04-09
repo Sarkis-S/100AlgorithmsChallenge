4777692c563f1e2592c6494fdb33bbfe3cc197ad
Sarkis-S <Sarkis.Sos19@gmail.com>
2020-04-08 20:37:32 -0700

    Complete caseInsensitivePalindrome no native reverse method

--- caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
+++ caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
@@ -4,10 +4,23 @@ function isCaseInsensitivePalindrome(inputString: string): boolean {
   // Finally, lowercase the letters and store in variable
   // Compare the inputString with the new variable
   // if they are the same return true, else false
-  
+
   const compare = inputString.split('').reverse().join('').toLowerCase();
 
   return (compare === inputString.toLowerCase()) ? true : false;
+}
+
+// Another solution without reverse method
+function isCaseInsensitivePalindrome(inputString: string): boolean {
+  let newInput = inputString.split('').join('').toLowerCase();
+  let compare: string[] = [];
+
+  for (let i = 0; i < newInput.length; i++) {
+    let current = newInput[i];
+    compare.unshift(current);
+  }
+
+  return (newInput === compare.join('')) ? true : false;
 }
 
 console.log(isCaseInsensitivePalindrome('AaBaa')); // true
