a071cf48f0f5230274541dc19ec70d37b56286cb
Sarkis-S <Sarkis.Sos19@gmail.com>
2020-04-08 20:00:03 -0700

    Complete caseInsensitivePalindrome.ts with native methods

--- caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
+++ caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
@@ -1,6 +1,14 @@
 function isCaseInsensitivePalindrome(inputString: string): boolean {
+  // Split the string into an array of letters
+  // Use reverse method, then join letters
+  // Finally, lowercase the letters and store in variable
+  // Compare the inputString with the new variable
+  // if they are the same return true, else false
+  
+  const compare = inputString.split('').reverse().join('').toLowerCase();
 
+  return (compare === inputString.toLowerCase()) ? true : false;
 }
 
-console.log(isCaseInsensitivePalindrome('AaBaa'));
-console.log(isCaseInsensitivePalindrome('abac'));
+console.log(isCaseInsensitivePalindrome('AaBaa')); // true
+console.log(isCaseInsensitivePalindrome('abac')); // false
