c57af9a407cf875838c88ed1367874c0920c2b41
Sarkis-S <Sarkis.Sos19@gmail.com>
2020-04-09 19:07:13 -0700

    Complete characterParity.ts

--- characterParity/characterParity.ts
+++ characterParity/characterParity.ts
@@ -1,7 +1,19 @@
 function characterParity(symbol: string): string {
-   
+  // Okay so the first thing to note is that the inputs are strings and will need to be parsed
+  // Create conditionals to return even, odd, or not a digit
+  // It's important to remember NaN is of type number in JavaScript
+
+  let parsed =  parseInt(symbol);
+  console.log(parsed);
+  if (isNaN(parsed)) {
+    return 'not a digit';
+  } else if (parsed % 2 === 0) {
+    return 'even';
+  } else {
+    return 'odd';
+  }
 }
 
-console.log(characterParity('5'))
-console.log(characterParity('8'))
-console.log(characterParity('q'))
+console.log(characterParity('5')); // odd
+console.log(characterParity('8')); // even
+console.log(characterParity('q')); // not a digit
