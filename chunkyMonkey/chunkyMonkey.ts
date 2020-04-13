4bc68f0ca09e7b1d4128e409ead8213105891330
Sarkis-S <Sarkis.Sos19@gmail.com>
2020-04-12 17:36:15 -0700

    Complete chunkeyMonkey.ts

--- chunkyMonkey/chunkyMonkey.ts
+++ chunkyMonkey/chunkyMonkey.ts
@@ -1,5 +1,26 @@
-function chunkyMonkey(arr: any[], size: number): any[][] {
+function chunkyMonkey(inputArray: any[], size: number): any[][] {
+  // So we're split the inputArray into "size" numnber of arrays. Simple enough. Seems we don't need to worry about odd-numbered sizes.
 
+  // Create an output array
+
+    // Loop through inputArray
+      // Create temp array
+      // Initialize size flag to zero
+
+      // Push current element into temp array
+      // Up size flag count
+      // If size flag count is equal to size
+        // Push temp array to output array
+
+  // Return output array
+
+  let output = [];
+
+  for (let i = 0; i < inputArray.length; i += size) {
+    output.push(inputArray.slice(i, i + size));
+  }
+
+  return output;
 }
 
 console.log(chunkyMonkey(["a", "b", "c", "d"], 2));
