427ec155a39273eec716ce6169a02d64736774c1
Sarkis-S <Sarkis.Sos19@gmail.com>
2020-04-21 20:33:03 -0700

    Complete compareIntegers.ts

--- compareIntegers/compareIntegers.ts
+++ compareIntegers/compareIntegers.ts
@@ -1,7 +1,22 @@
 function compareIntegers(a: string, b: string): string {
+  // Simple enough problem
 
+    // if a is equal to b
+      // return equal
+
+    // if a > b
+      // return greater
+
+    // return less
+    
+  const parsedA = parseInt(a);
+  const parsedB = parseInt(b);
+
+  if (parsedA === parsedB) return 'equal';
+  if (parsedA > parsedB) return 'greater';
+  return 'less';
 }
 
-console.log(compareIntegers('12', '13'));
-console.log(compareIntegers('875', '799'));
-console.log(compareIntegers('1000', '1000'));
+console.log(compareIntegers('12', '13')); // less
+console.log(compareIntegers('875', '799')); // greater
+console.log(compareIntegers('1000', '1000')); // equal
