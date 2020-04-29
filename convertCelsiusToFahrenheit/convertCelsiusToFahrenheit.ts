b4692308798d5a619722a6314fd45486c2d93fe5
Sarkis-S <Sarkis.Sos19@gmail.com>
2020-04-28 19:54:32 -0700

    Simplify the solution

--- convertCelsiusToFahrenheit/convertCelsiusToFahrenheit.ts
+++ convertCelsiusToFahrenheit/convertCelsiusToFahrenheit.ts
@@ -1,7 +1,10 @@
 function celsiusToFahrenheit(celsius: number): number {
+  // Given formula from Celsius to Fahrenheit:
+  // Temperature in Celsius * (9/5) + 32
 
+  return celsius * (9/5) + 32;
 }
 
-console.log(celsiusToFahrenheit(-30));
-console.log(celsiusToFahrenheit(-10));
-console.log(celsiusToFahrenheit(0));
+console.log(celsiusToFahrenheit(-30)); // -22
+console.log(celsiusToFahrenheit(-10)); // 14
+console.log(celsiusToFahrenheit(0)); // 32
