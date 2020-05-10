0b9864eee7d98b65aa689829115bf790c6b85daf
Sarkis-S <Sarkis.Sos19@gmail.com>
2020-05-10 12:39:54 -0700

    Refactor to remove nested loops

--- domainType/domainType.ts
+++ domainType/domainType.ts
@@ -14,35 +14,61 @@
     // Store the type of domain to output
 // Return the output
 
+// function domainType(domains: string[]): string[] {
+//   const typeDomains = {
+//     '.org': 'organiztion',
+//     '.com': 'commercial',
+//     '.net': 'network',
+//     '.info': 'information'
+//   }
+
+//   let grabDomains = [];
+//   let output = [];
+
+//   domains.forEach((domain) => {
+//     for (let i = domain.length; i > 0; i--) {
+//       let temp = '';
+
+//       if (domain[i] === '.') {
+//         temp += domain.slice(i);
+//         grabDomains.push(temp);
+//         break;
+//       }
+//     }
+//   });
+
+//   for (let i = 0; i < grabDomains.length; i++) {
+//     if (typeDomains.hasOwnProperty(grabDomains[i])) {
+//       output.push(typeDomains[grabDomains[i]]);
+//     }
+//   }
+
+//   return output;
+// }
+
+// There must be an easier way without nested loops
 function domainType(domains: string[]): string[] {
   const typeDomains = {
-    '.org': 'organiztion',
-    '.com': 'commercial',
-    '.net': 'network',
-    '.info': 'information'
-  }
-
-  let grabDomains = [];
-  let output = [];
-
-  domains.forEach((domain) => {
-    for (let i = domain.length; i > 0; i--) {
-      let temp = '';
-
-      if (domain[i] === '.') {
-        temp += domain.slice(i);
-        grabDomains.push(temp);
-        break;
+        'org': 'organiztion',
+        'com': 'commercial',
+        'net': 'network',
+        'info': 'information'
       }
-    }
-  });
 
-  for (let i = 0; i < grabDomains.length; i++) {
-    if (typeDomains.hasOwnProperty(grabDomains[i])) {
-      output.push(typeDomains[grabDomains[i]]);
-    }
+  const output = [];
+  let grabDomains = [];
+
+  for (let i = 0; i < domains.length; i++) {
+    const domain = domains[i].split('.');
+    grabDomains.push(domain[domain.length - 1]);
   }
 
+  for (let j = 0; j < grabDomains.length; j++) {
+    if (typeDomains.hasOwnProperty(grabDomains[j])) {
+      output.push(typeDomains[grabDomains[j]]);
+    }
+  }
+  
   return output;
 }
 
