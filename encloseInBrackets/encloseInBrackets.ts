24ad5d2a13af23d4dbc329f1cf07d1af715a6014
Sarkis-S <Sarkis.Sos19@gmail.com>
2020-05-15 22:49:52 -0700

    Complete enclosedInBrackets.ts

--- encloseInBrackets/encloseInBrackets.ts
+++ encloseInBrackets/encloseInBrackets.ts
@@ -1,5 +1,20 @@
-function encloseInBrackets(inputString: string): string {
+// We are to enclose the input string in a bracket
+// A string of my name 'Sarkis' should return '(Sarkis)'
 
+// Approach
+
+// So the easiest approach is to split the string
+// Then unshift the opening bracket and pushing the closing bracket
+
+// Finally return the joined version of the string
+
+function encloseInBrackets(inputString: string): string {
+  let newString = inputString.split('');
+
+  newString.unshift('(');
+  newString.push(')');
+
+  return newString.join('');
 }
 
 console.log(encloseInBrackets('abacaba'));
