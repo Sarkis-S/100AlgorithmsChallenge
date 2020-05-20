b4f4a47e4ab5584bd798ac186e0fa23e09c30597
Sarkis-S <Sarkis.Sos19@gmail.com>
2020-05-20 15:26:14 -0700

    Complete extractMatrixColumn.ts

--- extractMatrixColumn/extractMatrixColumn.ts
+++ extractMatrixColumn/extractMatrixColumn.ts
@@ -1,5 +1,15 @@
 function extractMatrixColumn(matrix: number[][], column: number): number[] {
+  let output = [];
 
+  matrix.forEach(array => {
+    for (let i = 0; i < array.length; i++) {
+      if (i === column) {
+        output.push(array[i]);
+      }
+    }
+  });
+
+  return output;
 }
 
-console.log(extractMatrixColumn([[1, 1, 1, 2], [0, 5, 0, 4], [2, 1, 3, 6]], 2));
+console.log(extractMatrixColumn([[1, 1, 1, 2], [0, 5, 0, 4], [2, 1, 3, 6]], 2)); // [1, 0, 3]
