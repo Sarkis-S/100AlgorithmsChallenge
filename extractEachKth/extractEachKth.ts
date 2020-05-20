204fabcb5e1608f508a0617530fec396dc20346c
Sarkis-S <Sarkis.Sos19@gmail.com>
2020-05-20 14:41:18 -0700

    Complete extractEachKth.ts

--- extractEachKth/extractEachKth.ts
+++ extractEachKth/extractEachKth.ts
@@ -1,4 +1,18 @@
 function extractEachKth(inputArray:number[], k: number): number[] {
+  // This is straightforward
+  let output = [];
+
+  inputArray.forEach(elem => {
+    if (elem !== k) {
+      output.push(elem);
+    }
+  });
+
+  return output;
+  
+  // filter method solution
+  // return inputArray.filter(elem => elem !== k);
 }
 
 console.log(extractEachKth([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3));
+// [1, 2, 4, 5, 6, 7, 8, 9, 10]
