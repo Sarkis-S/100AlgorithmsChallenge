466dcd92a5750b8fdd86e6aca618183db3e71c2b
Sarkis-S <Sarkis.Sos19@gmail.com>
2020-05-24 16:05:44 -0700

    Complete factorializeANumber.ts with while loop solution

--- factorializeANumber/factorializeANumber.ts
+++ factorializeANumber/factorializeANumber.ts
@@ -1,6 +1,24 @@
-function factorializeANumber(num: number): number {
+// How factorials work
+// 3 Factorial is written as 3!, which is 3 * 2 * 1
+// Given the number 'num' return the factorial of num
 
+// Approach
+// Most straightforward solution is a while loop multiplying num with num - 1
+// and storing that value for use later with the next iteration of num - 1
+// Rinse and repeat
+
+function factorializeANumber(num: number): number {
+  let count = num;
+
+  let factorial = count;
+
+  while (count > 1) {
+    factorial = (factorial * (count - 1));
+    count --;
+  }
+
+  return factorial;
 }
 
-console.log(factorializeANumber(5));
-console.log(factorializeANumber(10));
+console.log(factorializeANumber(5)); // 120
+console.log(factorializeANumber(10)); // 3628800
